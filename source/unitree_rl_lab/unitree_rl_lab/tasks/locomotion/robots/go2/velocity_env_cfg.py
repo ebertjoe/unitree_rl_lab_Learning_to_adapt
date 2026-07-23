76511276524f5bb40344cbfcf7a635bb45747fa0
Jonas Ebert <jonasebert2016@hotmail.com>
2026-07-23 17:36:38 +0200

    Code for 2026-07-23_17-35-41.pt

--- source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/go2/velocity_env_cfg.py
+++ source/unitree_rl_lab/unitree_rl_lab/tasks/locomotion/robots/go2/velocity_env_cfg.py
@@ -374,7 +374,7 @@ class RewardsCfg:
     # Eq.(8) - r_f will read env.beta_* references
     gait_tracking = RewTerm(
         func=mdp.r_f,
-        weight=-15.0,
+        weight=-17.5,
         params={
             "sensor_cfg": SceneEntityCfg(
                 "contact_forces",
